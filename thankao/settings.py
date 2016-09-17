fb06470bbbcbecaf05c5bf785fffba0e4a1f42c7
Watt Iamsuri <wattiamsuri@Watts-MacBook-Pro.local>
2016-09-17 19:12:46 +0700

    add templates

--- thankao/settings.py
+++ thankao/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'thankao.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + '/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,5 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticasset"),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
